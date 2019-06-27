4572ad1b76219643d6bd14e148cc0a769a9dd858
unpacker-auto-releaser <unpacker@auto.releaser>
2019-06-27 02:38:06 -0700

    Update unpacker-poller on Release: v

--- Formula/unpacker-poller.rb
+++ Formula/unpacker-poller.rb
@@ -1,1 +1,67 @@
+# Homebrew Formula Template. Built by Makefile: `make fomula`
+class UnpackerPoller < Formula
+  desc "Extracts Deluge downloads so Radarr or Sonarr may import them."
+  homepage "https://github.com/davidnewhall/unpacker-poller"
+  url "https://github.com/davidnewhall/unpacker-poller/archive/v0.4.0.tar.gz"
+  sha256 "f164f00b85cdefe6a9571ae3ece4a359ebf1e4d31307e1542cb8558851c0e474"
+  head "https://github.com/davidnewhall/unpacker-poller"
 
+  depends_on "go" => :build
+  depends_on "dep"
+
+  def install
+    ENV["GOPATH"] = buildpath
+
+    bin_path = buildpath/"src/github.com/davidnewhall/unpacker-poller"
+    # Copy all files from their current location (GOPATH root)
+    # to $GOPATH/src/github.com/davidnewhall/unpacker-poller
+    bin_path.install Dir["*"]
+    cd bin_path do
+      system "dep", "ensure", "-vendor-only"
+      system "make", "install", "VERSION=#{version}", "PREFIX=#{prefix}", "ETC=#{etc}"
+      # If this fails, the user gets a nice big warning about write permissions on their
+      # #{var}/log folder. The alternative could be letting the app silently fail
+      # to start when it cannot write logs. This is better. Fix perms; reinstall.
+      touch("#{var}/log/unpacker-poller.log")
+    end
+  end
+
+  def caveats
+    <<-EOS
+  Thanks for trying this app!
+    EOS
+  end
+
+  plist_options :startup => true
+
+  def plist
+    <<-EOS
+  <?xml version="1.0" encoding="UTF-8"?>
+  <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+  <plist version="1.0">
+     <dict>
+         <key>Label</key>
+         <string>#{plist_name}</string>
+         <key>ProgramArguments</key>
+         <array>
+             <string>#{bin}/unpacker-poller</string>
+             <string>-c</string>
+             <string>#{etc}/unpacker-poller/up.conf</string>
+         </array>
+         <key>RunAtLoad</key>
+         <true/>
+         <key>KeepAlive</key>
+         <true/>
+         <key>StandardErrorPath</key>
+         <string>#{var}/log/unpacker-poller.log</string>
+         <key>StandardOutPath</key>
+         <string>#{var}/log/unpacker-poller.log</string>
+     </dict>
+  </plist>
+    EOS
+  end
+
+  test do
+    assert_match "unpacker-poller v#{version}", shell_output("#{bin}/unpacker-poller -v 2>&1", 2)
+  end
+end
