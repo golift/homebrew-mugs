02e871c9597ae9f195247b726f40347eff59beb6
unifi-auto-releaser <unifi@auto.releaser>
2019-07-05 11:15:41 +0000

    Update unifi-poller on Release: v

--- Formula/unifi-poller.rb
+++ Formula/unifi-poller.rb
@@ -1,13 +1,10 @@
 # Homebrew Formula Template. Built by Makefile: `make fomula`
-require "language/go"
-
 class UnifiPoller < Formula
-  version "1.3.3"
-  sha256 "e4a23c2514d230e3b2bebbea1bd29f9224606e847826d1bcc22bb42bd772db12"
-  url "https://github.com/davidnewhall/unifi-poller/archive/v#{version}.tar.gz"
-  head "https://github.com/davidnewhall/unifi-poller"
-  desc "This daemon polls a Unifi controller at a short interval and stores the collected measurements in an Influx Database."
+  desc "Polls a UniFi controller and stores metrics in InfluxDB"
   homepage "https://github.com/davidnewhall/unifi-poller"
+  url "https://github.com/davidnewhall/unifi-poller/archive/v1.4.0.tar.gz"
+  sha256 "528aa75bc6b6b473cd294820d5f3a7a1af90d3eba9b10615ad109b2bc5b21a7e"
+  head "https://github.com/davidnewhall/unifi-poller"
 
   depends_on "go" => :build
   depends_on "dep"
@@ -20,48 +17,52 @@ class UnifiPoller < Formula
     # to $GOPATH/src/github.com/davidnewhall/unifi-poller
     bin_path.install Dir["*"]
     cd bin_path do
-      system "dep", "ensure"
+      system "dep", "ensure", "-vendor-only"
       system "make", "install", "VERSION=#{version}", "PREFIX=#{prefix}", "ETC=#{etc}"
       # If this fails, the user gets a nice big warning about write permissions on their
-      # [/usr/local/]var/log folder. The alternative could be letting the app silently
-      # fail to start when it cannot write logs. This is better. Fix perms; reinstall.
-      system "touch", "#{var}/log/unifi-poller.log"
+      # #{var}/log folder. The alternative could be letting the app silently fail
+      # to start when it cannot write logs. This is better. Fix perms; reinstall.
+      touch("#{var}/log/unifi-poller.log")
     end
   end
+
   def caveats
-    s = <<-EOS
+    <<-EOS
   This application will not work until the config file has authentication
-  information for a Unifi Controller and an Influx Database. Edit the config
+  information for a UniFi Controller and an Influx Database. Edit the config
   file at #{etc}/unifi-poller/up.conf then start the application with
   brew services start unifi-poller ~ log file: #{var}/log/unifi-poller.log
   The manual explains the config file options: man unifi-poller
     EOS
   end
 
-  def plist; <<-EOS
-   <?xml version="1.0" encoding="UTF-8"?>
-   <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-   <plist version="1.0">
-       <dict>
-           <key>Label</key>
-           <string>#{plist_name}</string>
-           <key>ProgramArguments</key>
-           <array>
-               <string>#{bin}/unifi-poller</string>
-               <string>-c</string>
-               <string>#{etc}/unifi-poller/up.conf</string>
-           </array>
-           <key>RunAtLoad</key>
-           <true/>
-           <key>KeepAlive</key>
-           <true/>
-           <key>StandardErrorPath</key>
-           <string>#{var}/log/unifi-poller.log</string>
-           <key>StandardOutPath</key>
-           <string>#{var}/log/unifi-poller.log</string>
-       </dict>
-   </plist>
-   EOS
+  plist_options :startup => true
+
+  def plist
+    <<-EOS
+  <?xml version="1.0" encoding="UTF-8"?>
+  <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+  <plist version="1.0">
+     <dict>
+         <key>Label</key>
+         <string>#{plist_name}</string>
+         <key>ProgramArguments</key>
+         <array>
+             <string>#{bin}/unifi-poller</string>
+             <string>-c</string>
+             <string>#{etc}/unifi-poller/up.conf</string>
+         </array>
+         <key>RunAtLoad</key>
+         <true/>
+         <key>KeepAlive</key>
+         <true/>
+         <key>StandardErrorPath</key>
+         <string>#{var}/log/unifi-poller.log</string>
+         <key>StandardOutPath</key>
+         <string>#{var}/log/unifi-poller.log</string>
+     </dict>
+  </plist>
+    EOS
   end
 
   test do
