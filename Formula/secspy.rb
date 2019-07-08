f8ae581902d89543e6bec5c812b0524c1d18efc2
secspy-auto-releaser <secspy@auto.releaser>
2019-07-08 07:09:51 +0000

    Update secspy on Release: 0.0.1

--- Formula/secspy.rb
+++ Formula/secspy.rb
@@ -1,13 +1,17 @@
-class Secpy < Formula
+# Homebrew Formula Template. Built by Makefile: `make fomula`
+class Secspy < Formula
   desc "Command Line Interface for SecuritySpy (IP Camera NVR)"
   homepage "https://github.com/davidnewhall/secspy"
   url "https://github.com/davidnewhall/secspy/archive/v0.0.1.tar.gz"
-  sha256 "3d4d632baa2a6a23c1ed9ea66f6b3e948fec691591b1b30186031075f415df30"
+  sha256 "d2510509d31a818764a465e1cb3239c32edf80c23f03caba41e4a90d469c56d4"
   head "https://github.com/davidnewhall/secspy"
+
   depends_on "go" => :build
   depends_on "dep"
+
   def install
     ENV["GOPATH"] = buildpath
+
     bin_path = buildpath/"src/github.com/davidnewhall/secspy"
     # Copy all files from their current location (GOPATH root)
     # to $GOPATH/src/github.com/davidnewhall/secspy
@@ -17,11 +21,13 @@ class Secpy < Formula
       system "make", "install", "VERSION=#{version}", "PREFIX=#{prefix}", "ETC=#{etc}"
     end
   end
+
   def caveats
     <<-EOS
     Good luck!
     EOS
   end
+
   test do
     assert_match "secspy v#{version}", shell_output("#{bin}/secspy -v 2>&1", 2)
   end
