79b069669a3d5524b6eeef3a0d55f45c4ce15181
secspy-auto-releaser <secspy@auto.releaser>
2019-07-09 07:07:06 +0000

    Update secspy on Release: 0.0.5-51

--- Formula/secspy.rb
+++ Formula/secspy.rb
@@ -2,8 +2,8 @@
 class Secspy < Formula
   desc "Command Line Interface for SecuritySpy (IP Camera NVR)"
   homepage "https://github.com/davidnewhall/secspy"
-  url "https://github.com/davidnewhall/secspy/archive/v0.0.4.tar.gz"
-  sha256 "e9c4130abf5d78b617ee6c5cce5462dd1805af0f950970334ab13e0d15ab2891"
+  url "https://github.com/davidnewhall/secspy/archive/v0.0.5.tar.gz"
+  sha256 "39c2293136adf5f5b79da7b892be5eb08c699b5c980097258f66dbbe36bb3b57"
   head "https://github.com/davidnewhall/secspy"
 
   depends_on "go" => :build
@@ -18,17 +18,17 @@ class Secspy < Formula
     bin_path.install Dir["*",".??*"]
     cd bin_path do
       system "dep", "ensure", "--vendor-only"
-      system "make", "install", "VERSION=#{version}", "ITERATION=47", "PREFIX=#{prefix}", "ETC=#{etc}"
+      system "make", "install", "VERSION=#{version}", "ITERATION=51", "PREFIX=#{prefix}", "ETC=#{etc}"
     end
   end
 
   def caveats
     <<-EOS
-    Good luck!
+    
     EOS
   end
 
   test do
-    assert_match "secspy v#{version}", shell_output("#{bin}/secspy -v 2>&1", 2)
+    assert_match "#{name} v#{version}", shell_output("#{bin}/#{name} -v 2>&1", 2)
   end
 end
