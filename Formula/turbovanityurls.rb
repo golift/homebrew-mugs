2f14b05a4b68e7df5681d4bf0850306a302ce828
turbovanityurls-auto-releaser <turbovanityurls@auto.releaser>
2019-07-22 22:41:06 +0000

    Update turbovanityurls on Release: v0.0.2-85

--- Formula/turbovanityurls.rb
+++ Formula/turbovanityurls.rb
@@ -5,8 +5,8 @@
 class Turbovanityurls < Formula
   desc "HTTP Server providing vanity go import paths."
   homepage "https://github.com/golift/turbovanityurls"
-  url "https://golift.io/turbovanityurls/archive/v0.0.1.tar.gz"
-  sha256 "fe0c17526ce63d40106eba4d405547f9a5af8cb83f58bf48500257c854ceab4d"
+  url "https://code.golift.io/turbovanityurls/archive/v0.0.2.tar.gz"
+  sha256 "7f6a8e4ea8d83a0322fd5273687bf7b37f945f88983aa9d6d3476a6142d8406f"
   head "https://github.com/golift/turbovanityurls"
 
   depends_on "go" => :build
@@ -21,7 +21,7 @@ class Turbovanityurls < Formula
     bin_path.install Dir["*",".??*"]
     cd bin_path do
       system "dep", "ensure", "--vendor-only"
-      system "make", "install", "VERSION=#{version}", "ITERATION=70", "PREFIX=#{prefix}", "ETC=#{etc}"
+      system "make", "install", "VERSION=#{version}", "ITERATION=85", "PREFIX=#{prefix}", "ETC=#{etc}"
       # If this fails, the user gets a nice big warning about write permissions on their
       # #{var}/log folder. The alternative could be letting the app silently fail
       # to start when it cannot write logs. This is better. Fix perms; reinstall.
