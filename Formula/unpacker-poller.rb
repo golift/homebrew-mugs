6a5248e952a7bf1900d9e21fa836bbb41db5ddf1
unpacker-poller-auto-releaser <unpacker-poller@auto.releaser>
2019-08-04 09:10:54 +0000

    Update unpacker-poller on Release: v0.5.2-98

--- Formula/unpacker-poller.rb
+++ Formula/unpacker-poller.rb
@@ -1,9 +1,12 @@
 # Homebrew Formula Template. Built by Makefile: `make fomula`
+# This is part of Application Builder.
+# https://github.com/golift/application-builder
+# This file is used when FORMULA is set to 'service'.
 class UnpackerPoller < Formula
   desc "Extracts Deluge downloads so Radarr or Sonarr may import them."
   homepage "https://github.com/davidnewhall/unpacker-poller"
-  url "https://golift.io/unpacker-poller/tar.gz/v0.5.1"
-  sha256 "b0988278ca76c43ef7750edc486a0e8279f550dca58869fdcb7efb4fce746889"
+  url "https://golift.io/unpacker-poller/archive/v0.5.2.tar.gz"
+  sha256 "8c547505cd6367592bf7755f03a09aff54fc89aacc85267baf906ee755a8e3b6"
   head "https://github.com/davidnewhall/unpacker-poller"
 
   depends_on "go" => :build
@@ -15,24 +18,26 @@ class UnpackerPoller < Formula
     bin_path = buildpath/"src/github.com/davidnewhall/unpacker-poller"
     # Copy all files from their current location (GOPATH root)
     # to $GOPATH/src/github.com/davidnewhall/unpacker-poller
-    bin_path.install Dir["*"]
+    bin_path.install Dir["*",".??*"]
     cd bin_path do
-      system "dep", "ensure", "-vendor-only"
-      system "make", "install", "VERSION=#{version}", "PREFIX=#{prefix}", "ETC=#{etc}"
+      system "dep", "ensure", "--vendor-only"
+      system "make", "install", "VERSION=#{version}", "ITERATION=98", "PREFIX=#{prefix}", "ETC=#{etc}"
       # If this fails, the user gets a nice big warning about write permissions on their
       # #{var}/log folder. The alternative could be letting the app silently fail
       # to start when it cannot write logs. This is better. Fix perms; reinstall.
-      touch("#{var}/log/unpacker-poller.log")
+      touch("#{var}/log/#{name}.log")
     end
   end
 
   def caveats
     <<-EOS
-  Thanks for trying this app!
+  Edit the config file at #{etc}/#{name}/up.conf then start #{name} with
+  brew services start #{name} ~ log file: #{var}/log/#{name}.log
+  The manual explains the config file options: man #{name}
     EOS
   end
 
-  plist_options :startup => true
+  plist_options :startup => false
 
   def plist
     <<-EOS
@@ -44,24 +49,24 @@ class UnpackerPoller < Formula
          <string>#{plist_name}</string>
          <key>ProgramArguments</key>
          <array>
-             <string>#{bin}/unpacker-poller</string>
-             <string>-c</string>
-             <string>#{etc}/unpacker-poller/up.conf</string>
+             <string>#{bin}/#{name}</string>
+             <string>--config</string>
+             <string>#{etc}/#{name}/up.conf</string>
          </array>
          <key>RunAtLoad</key>
          <true/>
          <key>KeepAlive</key>
          <true/>
          <key>StandardErrorPath</key>
-         <string>#{var}/log/unpacker-poller.log</string>
+         <string>#{var}/log/#{name}.log</string>
          <key>StandardOutPath</key>
-         <string>#{var}/log/unpacker-poller.log</string>
+         <string>#{var}/log/#{name}.log</string>
      </dict>
   </plist>
     EOS
   end
 
   test do
-    assert_match "unpacker-poller v#{version}", shell_output("#{bin}/unpacker-poller -v 2>&1", 2)
+    assert_match "#{name} v#{version}", shell_output("#{bin}/#{name} -v 2>&1", 2)
   end
 end
