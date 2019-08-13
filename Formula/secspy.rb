db965d5726ce372fe60e7dbebc899bced45e95c6
secspy-auto-releaser <secspy@auto.releaser>
2019-08-13 07:24:28 +0000

    Update secspy on Release: v0.0.21-81

--- Formula/secspy.rb
+++ Formula/secspy.rb
@@ -1,9 +1,12 @@
 # Homebrew Formula Template. Built by Makefile: `make fomula`
+# This is part of Application Builder.
+# https://github.com/golift/application-builder
+# This file is used when FORMULA is NOT set to 'service'.
 class Secspy < Formula
   desc "Command Line Interface for SecuritySpy (IP Camera NVR)"
   homepage "https://github.com/davidnewhall/secspy"
-  url "https://golift.io/secspy/tar.gz/v0.0.15"
-  sha256 "95a0d9e49c8ee522691230b8f95e74777afd53994ded4b72f683207ddaf8a69f"
+  url "https://golift.io/secspy/archive/v0.0.21.tar.gz"
+  sha256 "ff40a8a71f6f3952a8d6b8408b5464c0c71f31eaa8d708180aeaf39092bba44c"
   head "https://github.com/davidnewhall/secspy"
 
   depends_on "go" => :build
@@ -18,7 +21,7 @@ class Secspy < Formula
     bin_path.install Dir["*",".??*"]
     cd bin_path do
       system "dep", "ensure", "--vendor-only"
-      system "make", "install", "VERSION=#{version}", "ITERATION=73", "PREFIX=#{prefix}", "ETC=#{etc}"
+      system "make", "install", "VERSION=#{version}", "ITERATION=81", "PREFIX=#{prefix}", "ETC=#{etc}"
     end
   end
 
