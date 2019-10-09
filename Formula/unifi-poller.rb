43d7543f46457a6002279efb1387c25773565ad8
unifi-poller-auto-releaser <unifi-poller@auto.releaser>
2019-10-09 05:07:01 +0000

    Update unifi-poller on Release: v1.5.4-440

--- Formula/unifi-poller.rb
+++ Formula/unifi-poller.rb
@@ -5,8 +5,8 @@
 class UnifiPoller < Formula
   desc "Polls a UniFi controller and exports metrics to InfluxDB"
   homepage "https://github.com/davidnewhall/unifi-poller"
-  url "https://golift.io/unifi-poller/archive/v1.5.3.tar.gz"
-  sha256 "e711b08c4ee8c07a8598a36aa0b27017d3c48960e8b2eb88157db1442e8446da"
+  url "https://golift.io/unifi-poller/archive/v1.5.4.tar.gz"
+  sha256 "4f8c1e449c2b6292a1bf2899f1d464f4438719878d831cf336fb1daaab5aa6b9"
   head "https://github.com/davidnewhall/unifi-poller"
 
   depends_on "go" => :build
@@ -21,7 +21,7 @@ class UnifiPoller < Formula
     bin_path.install Dir["*",".??*"]
     cd bin_path do
       system "dep", "ensure", "--vendor-only"
-      system "make", "install", "VERSION=#{version}", "ITERATION=421", "PREFIX=#{prefix}", "ETC=#{etc}"
+      system "make", "install", "VERSION=#{version}", "ITERATION=440", "PREFIX=#{prefix}", "ETC=#{etc}"
       # If this fails, the user gets a nice big warning about write permissions on their
       # #{var}/log folder. The alternative could be letting the app silently fail
       # to start when it cannot write logs. This is better. Fix perms; reinstall.
