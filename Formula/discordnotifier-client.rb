9f4cb166e28568db07f9f2de05510461fa2e267a
discordnotifier-client-auto-releaser <discordnotifier-client@auto.releaser>
2020-12-20 02:32:33 +0000

    Update discordnotifier-client on Release: v0.0.1-alpha2-8

--- Formula/discordnotifier-client.rb
+++ Formula/discordnotifier-client.rb
@@ -16,7 +16,7 @@ class DiscordnotifierClient < Formula
     # Copy all files from their current location to buildpath/#{name}
     bin_path.install Dir["*",".??*"]
     cd bin_path do
-      system "make", "install", "VERSION=#{version}", "ITERATION=7", "PREFIX=#{prefix}", "ETC=#{etc}"
+      system "make", "install", "VERSION=#{version}", "ITERATION=8", "PREFIX=#{prefix}", "ETC=#{etc}"
       # If this fails, the user gets a nice big warning about write permissions on their
       # #{var}/log folder. The alternative could be letting the app silently fail
       # to start when it cannot write logs. This is better. Fix perms; reinstall.
