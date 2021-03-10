2edca75b0c78a7950d0cc6b92d1523e6f718bb46
discordnotifier-client-auto-releaser <discordnotifier-client@auto.releaser>
2021-03-10 05:39:16 +0000

    Update discordnotifier-client on Release: v0.0.7-37

--- Formula/discordnotifier-client.rb
+++ Formula/discordnotifier-client.rb
@@ -5,8 +5,8 @@
 class DiscordnotifierClient < Formula
   desc "Unified Client for DiscordNotifier.com"
   homepage "{{URL}}"
-  url "https://codeload.github.com/Go-Lift-TV/discordnotifier-client/tar.gz/v0.0.7-b3"
-  sha256 "898dd7e5e13792868b962244202dae40379ee2d1c2688b1cb1f263087f2cbadf"
+  url "https://codeload.github.com/Go-Lift-TV/discordnotifier-client/tar.gz/v0.0.7"
+  sha256 "68f48a2a44d5f049865e41b931d8744c390e0960b0bb1b7008ece0c25040b2f8"
   head "{{URL}}"
 
   depends_on "go" => :build
@@ -17,7 +17,7 @@ class DiscordnotifierClient < Formula
     # Copy all files from their current location to buildpath/#{name}
     bin_path.install Dir["*",".??*"]
     cd bin_path do
-      system "make", "install", "VERSION=#{version}", "ITERATION=34", "PREFIX=#{prefix}", "ETC=#{etc}"
+      system "make", "install", "VERSION=#{version}", "ITERATION=37", "PREFIX=#{prefix}", "ETC=#{etc}"
       # If this fails, the user gets a nice big warning about write permissions on their
       # #{var}/log folder. The alternative could be letting the app silently fail
       # to start when it cannot write logs. This is better. Fix perms; reinstall.
