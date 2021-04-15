3469f564e985093059e17489daa418345828c39f
secspy-auto-releaser <secspy@auto.releaser>
2021-04-15 11:05:11 +0000

    Update secspy on Release: v0.0.22-88

--- Formula/secspy.rb
+++ Formula/secspy.rb
@@ -4,24 +4,20 @@
 # This file is used when FORMULA is NOT set to 'service'.
 class Secspy < Formula
   desc "Command Line Interface for SecuritySpy (IP Camera NVR)"
-  homepage "https://github.com/davidnewhall/secspy"
-  url "https://golift.io/secspy/archive/v0.0.21.tar.gz"
-  sha256 "ff40a8a71f6f3952a8d6b8408b5464c0c71f31eaa8d708180aeaf39092bba44c"
-  head "https://github.com/davidnewhall/secspy"
+  homepage "{{URL}}"
+  url "https://codeload.github.com//davidnewhall/secspy/tar.gz/v0.0.22"
+  sha256 "fa2c3e297cb38c25e51895cd058be7d5e98b73941f47ddf9c7952add12925e45"
+  head "{{URL}}"
 
   depends_on "go" => :build
-  depends_on "dep"
+  depends_on "upx" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-
-    bin_path = buildpath/"src/github.com/davidnewhall/secspy"
-    # Copy all files from their current location (GOPATH root)
-    # to $GOPATH/src/github.com/davidnewhall/secspy
+    bin_path = buildpath/"#{name}"
+    # Copy all files from their current location to buildpath/#{name}
     bin_path.install Dir["*",".??*"]
     cd bin_path do
-      system "dep", "ensure", "--vendor-only"
-      system "make", "install", "VERSION=#{version}", "ITERATION=81", "PREFIX=#{prefix}", "ETC=#{etc}"
+      system "make", "install", "VERSION=#{version}", "ITERATION=88", "PREFIX=#{prefix}", "ETC=#{etc}"
     end
   end
 
