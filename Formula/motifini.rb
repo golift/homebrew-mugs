ae9e427dcb911bed95fff3672fb971064c958068
motifini-auto-releaser <motifini@auto.releaser>
2021-04-15 11:07:12 +0000

    Update motifini on Release: v0.0.6-77

--- Formula/motifini.rb
+++ Formula/motifini.rb
@@ -4,24 +4,20 @@
 # This file is used when FORMULA is set to 'service'.
 class Motifini < Formula
   desc "SecuritySpy-iMessage Integrator and Messages.app API"
-  homepage "https://github.com/davidnewhall/motifini"
-  url "https://codeload.github.com/davidnewhall/motifini/tar.gz/v0.0.5"
-  sha256 "b16f4b2221c75ea43794ac6b4cda557da1884ad3c84b038825a2796e00e07b08"
-  head "https://github.com/davidnewhall/motifini"
+  homepage "{{URL}}"
+  url "https://codeload.github.com/davidnewhall/motifini/tar.gz/v0.0.6"
+  sha256 "076d8573bedb882bd05a410bafddadac51ebe5321baa00f321034bfd0263f779"
+  head "{{URL}}"
 
   depends_on "go" => :build
-  depends_on "dep"
+  depends_on "upx" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-
-    bin_path = buildpath/"src/github.com/davidnewhall/motifini"
-    # Copy all files from their current location (GOPATH root)
-    # to $GOPATH/src/github.com/davidnewhall/motifini
+    bin_path = buildpath/"#{name}"
+    # Copy all files from their current location to buildpath/#{name}
     bin_path.install Dir["*",".??*"]
     cd bin_path do
-      system "dep", "ensure", "--vendor-only"
-      system "make", "install", "VERSION=#{version}", "ITERATION=66", "PREFIX=#{prefix}", "ETC=#{etc}"
+      system "make", "install", "VERSION=#{version}", "ITERATION=77", "PREFIX=#{prefix}", "ETC=#{etc}"
       # If this fails, the user gets a nice big warning about write permissions on their
       # #{var}/log folder. The alternative could be letting the app silently fail
       # to start when it cannot write logs. This is better. Fix perms; reinstall.
@@ -31,7 +27,7 @@ class Motifini < Formula
 
   def caveats
     <<-EOS
-  Edit the config file at #{etc}/motifini.conf then start #{name} with
+  Edit the config file at #{etc}/#{name}/motifini.conf then start #{name} with
   brew services start #{name} ~ log file: #{var}/log/#{name}.log
   The manual explains the config file options: man #{name}
     EOS
@@ -51,7 +47,7 @@ class Motifini < Formula
          <array>
              <string>#{bin}/#{name}</string>
              <string>--config</string>
-             <string>#{etc}/motifini.conf</string>
+             <string>#{etc}/#{name}/motifini.conf</string>
          </array>
          <key>RunAtLoad</key>
          <true/>
