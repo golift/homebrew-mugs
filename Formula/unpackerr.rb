6b6b4d27f52d08b5f0c576faae3cc3b35971c41e
unpackerr-auto-releaser <unpackerr@auto.releaser>
2021-09-09 20:14:03 +0000

    Update unpackerr on Release: v0.9.8-368

--- Formula/unpackerr.rb
+++ Formula/unpackerr.rb
@@ -5,8 +5,8 @@
 class Unpackerr < Formula
   desc "Extracts downloads so Radarr, Sonarr, Lidarr or Readarr may import them."
   homepage "{{URL}}"
-  url "https://golift.io/davidnewhall/unpackerr/archive/v0.9.7.tar.gz"
-  sha256 "7e8c6f473a57e5ab279bfc78fcf9f65c486c6f04bd9115428b3dc211af053761"
+  url "https://golift.io/davidnewhall/unpackerr/archive/v0.9.8.tar.gz"
+  sha256 "3f57742c3a22b1ad9d671931f2769964bc84c637dea427fd31ce65c7a8c256a2"
   head "{{URL}}"
 
   depends_on "go" => :build
@@ -17,7 +17,7 @@ class Unpackerr < Formula
     # Copy all files from their current location to buildpath/#{name}
     bin_path.install Dir["*",".??*"]
     cd bin_path do
-      system "make", "install", "VERSION=#{version}", "ITERATION=358", "PREFIX=#{prefix}", "ETC=#{etc}"
+      system "make", "install", "VERSION=#{version}", "ITERATION=368", "PREFIX=#{prefix}", "ETC=#{etc}"
       # If this fails, the user gets a nice big warning about write permissions on their
       # #{var}/log folder. The alternative could be letting the app silently fail
       # to start when it cannot write logs. This is better. Fix perms; reinstall.
