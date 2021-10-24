9441da6c5744e7b4c67a65006364aa34f6a73277
notifiarr-auto-releaser <notifiarr@auto.releaser>
2021-10-24 20:16:43 +0000

    Update notifiarr on Release: v0.2.0-144

--- Formula/notifiarr.rb
+++ Formula/notifiarr.rb
@@ -5,8 +5,8 @@
 class Notifiarr < Formula
   desc "Unified Client for Notifiarr.com"
   homepage "{{URL}}"
-  url "https://codeload.github.com/Notifiarr/notifiarr/tar.gz/v0.1.16"
-  sha256 "927c2b2c0453f73b78778b226602890198932e85ca3e035c08227b9240e765f3"
+  url "https://codeload.github.com/Notifiarr/notifiarr/tar.gz/v0.2.0"
+  sha256 "a911c1939edc92684036d6f392afe9ff6ea86fb52e923fdf9459f9de8fa62d61"
   head "{{URL}}"
 
   depends_on "go" => :build
@@ -17,7 +17,7 @@ class Notifiarr < Formula
     # Copy all files from their current location to buildpath/#{name}
     bin_path.install Dir["*",".??*"]
     cd bin_path do
-      system "make", "install", "VERSION=#{version}", "ITERATION=166", "PREFIX=#{prefix}", "ETC=#{etc}"
+      system "make", "install", "VERSION=#{version}", "ITERATION=144", "PREFIX=#{prefix}", "ETC=#{etc}"
       # If this fails, the user gets a nice big warning about write permissions on their
       # #{var}/log folder. The alternative could be letting the app silently fail
       # to start when it cannot write logs. This is better. Fix perms; reinstall.
